7f1f88e858dc56843984bef389c265bf77ac90c8
Joakim Tengstrand <joakimtengstrand@gmail.com>
2017-11-18 14:51:43 +0100

    Changed description.

--- clojure/leiningen-plugin/project.clj
+++ clojure/leiningen-plugin/project.clj
@@ -1,5 +1,5 @@
 (defproject polylith.leiningen "0.1-SNAPSHOT"
-            :description "Polylith tools"
+            :description "Polylith Leiningen plugin"
             :url "https://github.com/tengstrand/polylith"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
