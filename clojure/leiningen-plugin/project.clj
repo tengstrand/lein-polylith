81707d425ddaf71b1b77a528f76a6aaec995b4d3
Joakim Tengstrand <joakimtengstrand@gmail.com>
2017-11-18 15:31:42 +0100

    Added "Delete me" to the description.

--- clojure/leiningen-plugin/project.clj
+++ clojure/leiningen-plugin/project.clj
@@ -1,5 +1,5 @@
 (defproject polylith.leiningen "0.1-SNAPSHOT"
-            :description "Polylith Leiningen plugin"
+            :description "Delete me"
             :url "https://github.com/tengstrand/polylith"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
