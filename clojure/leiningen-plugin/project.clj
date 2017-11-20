3e287ccf2fbc06b173ce6fbd8ed1c02478e06d03
Joakim Tengstrand <joakimtengstrand@gmail.com>
2017-11-20 12:04:08 +0100

    Changed back the project name.

--- clojure/leiningen-plugin/project.clj
+++ clojure/leiningen-plugin/project.clj
@@ -1,6 +1,7 @@
-(defproject polylith/leiningen "0.1-SNAPSHOT"
-            :description "Leiningen Polylith plugin"
+(defproject polylith/lein-polylith "0.1-SNAPSHOT"
+            :description "Polylith, a component based architecture"
             :url "https://github.com/tengstrand/polylith"
+            :dependencies [[selmer "1.11.3"]]
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :eval-in-leiningen true)
