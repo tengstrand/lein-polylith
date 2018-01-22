1e368ce286bd343eeceb769003109c1182b5c0aa
Joakim Tengstrand <joakimtengstrand@gmail.com>
2018-01-22 16:49:33 +0100

    Changed back the project file so that it push to Clojars again.

--- clojure/leiningen-plugin/project.clj
+++ clojure/leiningen-plugin/project.clj
@@ -1,16 +1,10 @@
-(defproject com.github.tengstrand/lein-polylith "0.0.27-alpha"
-  :description "Polylith - a tool for building component based architectures, by Joakim Tengstrand."
+(defproject polylith/lein-polylith "0.0.27-alpha"
+  :description "Polylith - a component based architecture, by Joakim Tengstrand."
+  :repositories [["releases" {:url "https://blueant.com/archiva/internal"
+                              :creds :gpg}]]
   :url "https://github.com/tengstrand/polylith"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/tengstrand/polylith"}
-  :eval-in-leiningen true
-  :pom-addition [:developers [:developer
-                              [:name "Joakim Tengstrand"]
-                              [:email "joakim.tengstrand@gmail.com"]
-                              [:timezone "+1"]]]
-  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                    :creds :gpg}
-                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                     :creds :gpg}})
+  :eval-in-leiningen true)
