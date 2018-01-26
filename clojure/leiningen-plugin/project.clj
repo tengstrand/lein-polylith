d4b19aefa7675063038587979d328820e8e8277f
Joakim Tengstrand <joakimtengstrand@gmail.com>
2018-01-26 06:11:30 +0100

    Removed :repositories.

--- clojure/leiningen-plugin/project.clj
+++ clojure/leiningen-plugin/project.clj
@@ -1,7 +1,5 @@
 (defproject polylith/lein-polylith "0.0.30-alpha"
   :description "Polylith - a component based architecture, by Joakim Tengstrand."
-  :repositories [["releases" {:url "https://blueant.com/archiva/internal"
-                              :creds :gpg}]]
   :url "https://github.com/tengstrand/polylith"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
