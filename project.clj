96df1dd0454a052c44f3cf7227a3795d0de9e25f
Furkan Bayraktar <me@furkanbayraktar.com>
2018-04-15 17:18:58 +0200

    Updated project github url.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject polylith/lein-polylith "0.0.35-alpha"
   :description "Polylith - a component based architecture, by Joakim Tengstrand."
-  :url "https://github.com/tengstrand/polylith"
+  :url "https://github.com/tengstrand/lein-polylith"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "https://github.com/tengstrand/polylith"}
+        :url  "https://github.com/tengstrand/lein-polylith"}
   :eval-in-leiningen true)
