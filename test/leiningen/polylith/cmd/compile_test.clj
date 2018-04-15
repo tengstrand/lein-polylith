aa809dc3bd5722c64ace87c429cd4fa4824619dd
Furkan Bayraktar <me@furkanbayraktar.com>
2018-04-15 19:27:38 +0200

    Added tests for compile command.

---
+++ test/leiningen/polylith/cmd/compile_test.clj
@@ -0,0 +1,31 @@
+(ns leiningen.polylith.cmd.compile-test
+  (:require [clojure.test :refer :all]
+            [leiningen.polylith.cmd.test-helper :as helper]
+            [leiningen.polylith.file :as file]
+            [leiningen.polylith :as polylith]))
+
+(use-fixtures :each helper/test-setup-and-tear-down)
+
+(defn fake-fn [& args]
+  args)
+
+(deftest polylith-test--with-print-argument--print-tests
+  (with-redefs [file/current-path (fn [] @helper/root-dir)
+                leiningen.polylith.cmd.compile/sh fake-fn]
+    (let [ws-dir (str @helper/root-dir "/ws1")
+          project (helper/settings ws-dir "my.company")
+          output (with-out-str
+                   (polylith/polylith nil "create" "w" "ws1" "my.company")
+                   (polylith/polylith (helper/settings ws-dir "my.company")
+                                      "create" "c" "comp1")
+                   (polylith/polylith project "compile"))]
+      (is (= (str "\n"
+                  "Changed components: comp1\n"
+                  "Changed bases:\n"
+                  "Changed systems:\n"
+                  "\n"
+                  "Compiling interfaces\n"
+                  "(lein install :dir " ws-dir "/interfaces)\n"
+                  "Compiling components/comp1\n"
+                  "(lein compile :dir " ws-dir "/components/comp1)\n")
+             output)))))
