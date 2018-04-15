19e5ad52ac27e30842ff82f6c7148135dcb018cb
Furkan Bayraktar <me@furkanbayraktar.com>
2018-04-15 19:24:28 +0200

    Fixed tests for test command

--- test/leiningen/polylith/cmd/test_test.clj
+++ test/leiningen/polylith/cmd/test_test.clj
@@ -6,35 +6,20 @@
 
 (use-fixtures :each helper/test-setup-and-tear-down)
 
-(defn fake-fn [_ _ changes]
-  (if (empty? changes)
-    []
-    ['comp1.a-test 'comp1.another-test]))
+(defn fake-fn [& args]
+  args)
 
 (deftest polylith-test--with-print-argument--print-tests
   (with-redefs [file/current-path (fn [] @helper/root-dir)
-                leiningen.polylith.cmd.test/tests fake-fn]
+                leiningen.polylith.cmd.test/sh fake-fn]
     (let [ws-dir (str @helper/root-dir "/ws1")
           project (helper/settings ws-dir "my.company")
           output (with-out-str
                    (polylith/polylith nil "create" "w" "ws1" "my.company")
                    (polylith/polylith (helper/settings ws-dir "my.company")
                                       "create" "c" "comp1")
-                   (polylith/polylith project "test" "+"))]
-      (is (= (str "  comp1.a-test\n"
-                  "  comp1.another-test\n")
-             output)))))
-
-(deftest polylith-test--with-leiningen-argument--print-leiningen-execution-statement
-  (with-redefs [file/current-path (fn [] @helper/root-dir)
-                leiningen.polylith.cmd.diff/do-diff (fn [_ _] helper/diff)
-                leiningen.polylith.cmd.test/tests fake-fn]
-    (let [ws-dir (str @helper/root-dir "/ws1")
-          project (helper/settings ws-dir "my.company")
-          output (with-out-str
-                   (polylith/polylith nil "create" "w" "ws1" "my.company")
-                   (polylith/polylith (helper/settings ws-dir "my.company")
-                                      "create" "c" "comp1")
-                   (polylith/polylith project "test" "-"))]
-      (is (= "lein test comp1.a-test comp1.another-test\n"
+                   (polylith/polylith project "test"))]
+      (is (= (str "Start execution of 1 tests:\n"
+                  "lein test my.company.comp1.core-test\n"
+                  "(lein test my.company.comp1.core-test :dir " ws-dir "/environments/development)\n")
              output)))))
