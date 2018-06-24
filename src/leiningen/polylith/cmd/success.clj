41456811358255e24b9004c81d8a4003c9c7b0c2
Furkan Bayraktar <me@furkanbayraktar.com>
2018-06-24 12:42:57 +0200

    Fixed bookmark check in success command.

--- src/leiningen/polylith/cmd/success.clj
+++ src/leiningen/polylith/cmd/success.clj
@@ -3,10 +3,15 @@
             [leiningen.polylith.git :as git]
             [leiningen.polylith.time :as time]))
 
+(defn bookmark? [ws-path bookmark]
+  (when-not (nil? bookmark)
+    (let [[timestamp? _] (time/parse-timestamp bookmark)]
+      (not (or timestamp? (git/valid-sha1? ws-path bookmark))))))
+
 (defn execute [ws-path [bookmark]]
-  (let [bmark (if (nil? bookmark)
-                :last-successful-build
-                (keyword bookmark))]
+  (let [bookmark (if (bookmark? ws-path bookmark)
+                   (keyword bookmark)
+                   :last-successful-build)]
     (if (shared/ci?)
-      (git/set-bookmark! ws-path bmark)
-      (time/set-bookmark! ws-path bmark))))
+      (git/set-bookmark! ws-path bookmark)
+      (time/set-bookmark! ws-path bookmark))))
