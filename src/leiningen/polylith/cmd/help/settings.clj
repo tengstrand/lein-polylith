ad9eabc8987948c40d4511e7e2666db016476e05
Joakim Tengstrand <joakimtengstrand@gmail.com>
2018-07-14 11:15:40 +0200

    Updated help text.

--- src/leiningen/polylith/cmd/help/settings.clj
+++ src/leiningen/polylith/cmd/help/settings.clj
@@ -1,7 +1,7 @@
 (ns leiningen.polylith.cmd.help.settings)
 
 (defn help []
-  (println "  Show project settings various information.")
+  (println "  Show project settings with various information.")
   (println)
   (println "  examples:")
   (println "    lein polylith settings"))
