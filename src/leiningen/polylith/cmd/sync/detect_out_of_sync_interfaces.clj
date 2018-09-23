7273bd666a6344606e48085d9b9654e6598b38e7
Joakim Tengstrand <joakimtengstrand@gmail.com>
2018-09-23 09:07:50 +0200

    Simplified the error message.

--- src/leiningen/polylith/cmd/sync/detect_out_of_sync_interfaces.clj
+++ src/leiningen/polylith/cmd/sync/detect_out_of_sync_interfaces.clj
@@ -28,7 +28,7 @@
 (defn read-code [path]
   (filterv def? (drop 1 (file/read-file path))))
 
-(defn signatures [interface src-code]
+(defn signatures [src-code]
   "Takes the source code of a def, function or macro
    and returns a list with the signatures."
   (let [type (first src-code)
@@ -37,42 +37,42 @@
                                    (vector? %)))
                          src-code)]
     (if (= 'def type)
-      #{[interface type name 0]}
+      #{[type name 0]}
       (if (vector? (first code))
-        #{[interface type name (-> code first count)]}
-        (set (map #(vector interface type name (-> % first count)) code))))))
+        #{[type name (-> code first count)]}
+        (set (map #(vector type name (-> % first count)) code))))))
 
 (defn def-str [name]
   (str "\n(def " name ")"))
 
 (defn ifc-set [defs]
-  (set (map second defs)))
+  (set (map first defs)))
 
-(defn missing-defs [interface interface-path component-path]
+(defn missing-defs [interface-path component-path]
   (let [interface-code (read-code interface-path)
         component-code (read-code component-path)
-        interface-defs (set (mapcat #(signatures interface %) interface-code))
-        component-defs (set (mapcat #(signatures interface %) component-code))
+        interface-defs (set (mapcat signatures interface-code))
+        component-defs (set (mapcat signatures component-code))
         missing (set/difference component-defs interface-defs)
         already-defined (set/intersection (ifc-set missing) (ifc-set interface-defs))]
     {:missing missing
-     :already-defined (filter #(contains? already-defined (second %)) missing)}))
+     :already-defined (filter #(contains? already-defined (first %)) missing)}))
 
-(defn error-message [[interface type name arity]]
+(defn error-message [[type name arity]]
   (cond
-    (= type 'defn) (str "\"function '" name "' with arity " arity " in workspace interface '" interface "' must be added manually\"")
-    (= type 'defmacro) (str "\"macro '" name "' with arity " arity " in workspace interface '" interface "' must be added manually\"")
+    (= type 'defn) (str "\"function '" name "' with arity " arity " must be added manually\"")
+    (= type 'defmacro) (str "\"macro '" name "' with arity " arity " must be added manually\"")
     :else (str name)))
 
 (defn wspath [ws path]
   (let [index (str/index-of path (str "/" ws "/"))]
     (subs path (+ index (count ws) 2))))
 
-(defn missing-definitions [ws interface interface-path component-path]
+(defn missing-definitions [ws interface-path component-path]
   (if (not (file/file-exists component-path))
     [false (str "Expected to find interface '" component-path "'.")]
     (let [path (wspath ws interface-path)
-          {:keys [missing already-defined]} (missing-defs interface interface-path component-path)]
+          {:keys [missing already-defined]} (missing-defs interface-path component-path)]
       (if (not (empty? already-defined))
         [false (str "Workspace interfaces are out of sync in '" path "': "
                     (str/join ", " (map error-message already-defined)))]
@@ -80,10 +80,9 @@
 
 (defn sync-interface! [ws-path ws top-dir ifc->components sub-path]
   (let [ns-path (if (str/blank? top-dir) "" (str "/" top-dir))
-        interface (first (str/split sub-path #"/"))
         interface-path (str ws-path "/interfaces/src" ns-path "/" sub-path)
         paths (->component-paths ws-path top-dir sub-path ifc->components)
-        results (mapv #(missing-definitions ws interface interface-path %) paths)
+        results (mapv #(missing-definitions ws interface-path %) paths)
         errors (str/join ", " (mapv second (filter (complement first) results)))]
     (if (not (empty? errors))
       (do
@@ -116,7 +115,7 @@
     (doseq [sub-path sub-paths]
       (sync-interface! ws-path ws top-dir ifc->components sub-path))))
 
-;(def ws-path "/Users/joakimtengstrand/IdeaProjects/ws22")
-;(def top-dir "com/abc")
-;
-;(sync-interfaces! ws-path top-dir)
+(def ws-path "/Users/joakimtengstrand/IdeaProjects/ws22")
+(def top-dir "com/abc")
+
+(sync-interfaces! ws-path top-dir)
