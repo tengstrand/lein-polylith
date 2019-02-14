da98de554d4fd89b3c02eb452bdf0c954c70e906
Joakim Tengstrand <joakimtengstrand@gmail.com>
2019-02-14 10:51:37 +0100

    Sync system libraries based on the components (and bases) they depend on. (#28)

--- src/leiningen/polylith/cmd/sync/systems.clj
+++ src/leiningen/polylith/cmd/sync/systems.clj
@@ -16,7 +16,8 @@
             project-path (str system-path "/project.clj")
             path (str "systems/" system "/project.clj")
             src-path (str system-path "/src/" top-dir)
-            entities (file/directory-names src-path)
+            directories (file/directories src-path)
+            entities (mapv #(shared/link->entity ws-path %) directories)
             libs (environments/entities-libs ws-path [] entities components)
             sys-libs (sort-by first (shared/libraries project-path))
             content (seq (updated-system-content libs project-path))]
